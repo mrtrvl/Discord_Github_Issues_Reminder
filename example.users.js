f14bac20bc5f7e08b23347f6cf0899bdd9d4df6e
mrtrvl <mrt@mrt.ee>
2023-04-26 13:37:39 +0300

    Create example.users.js

---
+++ example.users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    firstName: 'Mati',
+    lastName: 'Maasikas',
+    email: '',
+    githubUsername: 'matimaasikas',
+    discordId: '111122223333444455',
+    curriculum: '',
+  },
+  {
+    id: 2,
+    firstName: 'Tiina',
+    lastName: 'Tibutav',
+    email: '',
+    githubUsername: 'tiinatibutav',
+    discordId: '22223333555566',
+    curriculum: '',
+  },
+];
+
+module.exports = users;
